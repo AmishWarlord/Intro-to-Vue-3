071250238484e03d71c6c1a90cac231c7160e816
Charles Fry <52051562+AmishWarlord@users.noreply.github.com>
2021-01-12 11:57:51 -0500

    finished module 2

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
-const product = 'Socks'
+const app = Vue.createApp({
+    data: function() {
+        return {
+            product: 'waffles',
+            description: 'very cool, Kanye'
+        }
+    }
+})
